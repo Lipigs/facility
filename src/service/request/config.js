6a792191faf93c09ead7270075341c85865457dd
Lipigs <93815919+Lipigs@users.noreply.github.com>
2024-01-29 00:47:04 +0800

    Add files via upload

---
+++ src/service/request/config.js
@@ -0,0 +1,9 @@
+
+
+// 服务器
+   export const BASE_URL ="https://facility2-89788-7-1307807477.sh.run.tcloudbase.com";
+  
+//  本地
+
+// export const BASE_URL = "http://localhost:19001/";
+export const OUTTIME = 5000;
